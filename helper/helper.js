40b6cef21c6351c5b565f9c78f20a4eecdc9298d
ppmaungdevv <ppmaungdev@gmail.com>
2025-12-20 09:44:32 +0700

    fixed a classic JavaScript "gotcha": String Concatenation bug

--- helper/helper.js
+++ helper/helper.js
@@ -15,12 +15,12 @@ export const compareHashedPassword = async (pwd, hashed_pwd) => {
   return is_match
 }
 
-export const getAccessTokenData = async ( payload, ttl = '1h' ) => {
+export const getAccessTokenData = async ( payload, ttl = '60m' ) => {
   payload.jti = uuidv4()
   const token = jwt.sign(
     payload,
     process.env.JWT_SECRET,
-    { expiresIn: process.env.ATOKEN_LIFESPAN_MINS || ttl }
+    { expiresIn: `${process.env.ATOKEN_LIFESPAN_MINS}m` || ttl }
   )
 
   const exp = new Date(Date.now() + (process.env.ATOKEN_LIFESPAN_MINS * 60 * 1000))
@@ -33,7 +33,8 @@ export const getRefreshTokenData = async () => {
   const refresh_token = uuidv4()
   
   const expires_at = new Date()
-  expires_at.setDate(expires_at.getDate() + process.env.RTOKEN_LIFESPAN_DAYS).toISOString
+  // expires_at.setDate(expires_at.getDate() + process.env.RTOKEN_LIFESPAN_DAYS).toISOString // a classic JavaScript "gotcha": String Concatenation.
+  expires_at.setDate(expires_at.getDate() + Number(process.env.RTOKEN_LIFESPAN_DAYS)).toISOString
 
   const data = {
     token: refresh_token,
